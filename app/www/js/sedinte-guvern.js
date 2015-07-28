02815818149be00ff1c1f491032e8bf0c092370e
Iulia Ionita <iulia@ionita.me>
2015-07-28 14:29:30 +0300

    Adaugat link cu 'Vezi sedinta pe site' pe pagina de sedinte-guvern

--- app/www/js/sedinte-guvern.js
+++ app/www/js/sedinte-guvern.js
@@ -29,18 +29,19 @@
 		  jsonpCallback: 'callback',
 		  type: 'GET',
 		  }).done(function ( data ) {
+		  	console.log(data);
 		   $.each(data.rez, function(i, item){
 		   	var date = new Date(item.data_publicarii)
 		   	var formattedDate = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
 		   	// vezi daca exista container pentru obiect in functie de data publicarii
 		   	if ($('.'+formattedDate).length > 0) {
 		   		// daca da, adauga la finalul containerului
-		   		content = '<div class="container-sedinte__item__container" data-role="collapsible"><h3>' + item.titlu +'</h3><p>'+ item.continut + '</p></div>'
+		   		content = '<div class="container-sedinte__item__container" data-role="collapsible"><h3>' + item.titlu +'</h3><p><a href="'+ item.url +'">Vezi ședința pe site</a>'+ item.continut + '</p></div>'
 				$('.'+formattedDate).append(content);
 				$('.container-sedinte').collapsibleset('refresh')
 		   	} else {
 			   	// daca nu, creaza un nou container cu data publicarii ca si clasa
-			   	content = '<div class="container-sedinte__item ' + formattedDate + '"><h2> Ședința din ' + formattedDate + '</h2><div class="container-sedinte__item__container" data-role="collapsible"> <h3>' + item.titlu +'</h3><p>'+ item.continut +'</p> </div></div>'
+			   	content = '<div class="container-sedinte__item ' + formattedDate + '"><h2> Ședința din ' + formattedDate + '</h2><div class="container-sedinte__item__container" data-role="collapsible"> <h3>' + item.titlu +'</h3><p><a href="'+ item.url +'">Vezi ședința pe site</a>'+ item.continut +'</p> </div></div>'
 			   	$('.container-sedinte').append(content);
 				$('.container-sedinte').collapsibleset('refresh')
 
