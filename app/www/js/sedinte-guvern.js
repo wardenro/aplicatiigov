7e4bae7f833ee1861bd28ad58e50c9b21fb62336
Iulia Ionita <iulia@ionita.me>
2015-07-28 13:00:19 +0300

    Logica pentru formatarea datii afisate pe sedinte-guvern

--- app/www/js/sedinte-guvern.js
+++ app/www/js/sedinte-guvern.js
@@ -30,17 +30,18 @@
 		  type: 'GET',
 		  }).done(function ( data ) {
 		   $.each(data.rez, function(i, item){
-
-		   	var date = item.data_publicarii.substring(0, item.data_publicarii.indexOf(' '))
+		   	var date = new Date(item.data_publicarii)
+		   	var dateISO = date.toISOString();
+		   	var formattedDate = date.getDate() + '-' + date.getMonth() + '-' + date.getFullYear();
 		   	// vezi daca exista container pentru obiect in functie de data publicarii
-		   	if ($('.'+date).length > 0) {
+		   	if ($('.'+dateISO).length > 0) {
 		   		// daca da, adauga la finalul containerului
 		   		content = '<div class="container-sedinte__item__container" data-role="collapsible"><h3>' + item.titlu +'</h3><p>'+ item.continut + '</p></div>'
-				$('.'+date).append(content);
+				$('.'+dateISO).append(content);
 				$('.container-sedinte').collapsibleset('refresh')
 		   	} else {
 			   	// daca nu, creaza un nou container cu data publicarii ca si clasa
-			   	content = '<div class="container-sedinte__item ' + date + '"><h2> Ședința din ' + date + '</h2><div class="container-sedinte__item__container" data-role="collapsible"> <h3>' + item.titlu +'</h3><p>'+ item.continut +'</p> </div></div>'
+			   	content = '<div class="container-sedinte__item ' + dateISO + '"><h2> Ședința din ' + formattedDate + '</h2><div class="container-sedinte__item__container" data-role="collapsible"> <h3>' + item.titlu +'</h3><p>'+ item.continut +'</p> </div></div>'
 			   	$('.container-sedinte').append(content);
 				$('.container-sedinte').collapsibleset('refresh')
 
