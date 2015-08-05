12cc159e8caffd645c68bb2a26c279896dfc4156
Iulia Ionita <iulia@ionita.me>
2015-08-05 14:37:30 +0300

    Mici schimbari la design

---
+++ www/js/ministri.js
@@ -0,0 +1,18 @@
+
+myurl = 'http://gov.ro/ro/json-ministri';
+$.ajax({
+  dataType: "jsonp",
+  url: myurl,
+  jsonpCallback: 'callback',
+  type: 'GET',
+  }).done(function ( data ) {
+   $.each(data.rez, function(i, item){
+
+   content = '<div data-role="collapsible"><h3 >' + '<div class="nume">' + item.prenume + ' ' + item.nume + "</div>" + '<img class="resize" src="' + item.image + '"/>'  + '<div class="functie">' + item.functie + '</div></h3>' + '<p><div class="descriere">' + item.descriere.replace(/\n/g,'<br />') + '</div></p> </div>' 
+ 
+   $('.container-ministri').append(content)
+
+   $('.container-ministri').collapsibleset('refresh')
+
+   })
+  });
