1ae15c845cc1228f9c07959b5f52ea061bc6e647
mihaelafronie <mihaela.fronie@yahoo.com>
2015-08-25 16:01:35 +0300

    institutii new page

--- www/js/institutii.js
+++ www/js/institutii.js
@@ -9,29 +9,21 @@ $.ajax({
   $.each(data.rez, function(i, item){
 	
    content = '<h3>' + '<div class="nume">' + item.titlu + '</div></h3>' +
-   '<p> <div class="link"><a href="#" onclick="window.open(\''+ 
-			   	item.link + '\', \'_system\');" target="_blank">'+item.link+'</a></div></p>';
+   '<p> <div class="link"><a href="#" onclick="window.open(\''+item.link + '\');">'+item.link+'</a></div></p>';
 	if(item.facebook!="")
 	{
-		content=content +'<a href="#" onclick="window.open(\''+ 
-			   	item.facebook + '\', \'_system\'); "target="_blank">'+'<img class="resize" src="images/social_facebook_box_blue.png"; style="width:30px;height:30px"</a></div>';
+		content=content +'<a href="#" onclick="window.open(\''+item.facebook + '\'); ">'+'<img class="resize" src="images/social_facebook_box_blue.png"; style="width:30px;height:30px"</a></div>';
 	}		
   
 	if(item.youtube!="")
 	{
-		content=content +'<a href="#" onclick="window.open(\''+ 
-			   	item.youtube + '\', \'_system\'); "target="_blank">'+'<img class="resize" src="images/YouTube-icon.png" style="width:30px;height:30px"</a></div>';
+		content=content +'<a href="#" onclick="window.open(\''+item.youtube + '\'); ">'+'<img class="resize" src="images/YouTube-icon.png" style="width:30px;height:30px"</a></div>';
 	}		
 	if(item.twitter!="")
 	{
-		content=content+'<a href="#" onclick="window.open(\''+ 
-			   	item.twitter + '\', \'_system\'); "target="_blank">'+'<img class="resize" src="images/Twitter_Logo_Hd_Png_03.png" style="width:30px;height:30px"</a></div>';
+		content=content+'<a href="#" onclick="window.open(\''+item.twitter + '\'); ">'+'<img class="resize" src="images/Twitter_Logo_Hd_Png_03.png" style="width:30px;height:30px"</a></div>';
 	}		
   
-  
-  
-  //<a href="#" onclick="window.open(\'http://gov.ro/ro/guvernul/sedinte-guvern/'+ 
-			   	//item.url + '\', \'_system\');">Vezi ședința pe site</a><p>'
 	$(document).on("collapsibleexpand", "[data-role=collapsible]", function () 
 	{
 	  var position = $(this).offset().top - 60;
