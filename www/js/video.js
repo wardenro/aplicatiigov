901837f097c3c204743a6451f39b800a7d72371c
Iulia Ionita <iulia@ionita.me>
2015-08-25 16:41:38 +0300

    Rezolvat linkul de pe pagina de video

--- www/js/video.js
+++ www/js/video.js
@@ -7,10 +7,8 @@ $.ajax({
   }).done(function ( data ) {
    $.each(data.items, function(i, item){
    content =  '<center><div class="nume">'
-   + '<a href="https://www.youtube.com/watch?v=' + item.snippet.resourceId.videoId + '&list=PLVvoFIJvWUtYvBbvnq7dZUzza3cyFLv-2">' 
+   + '<a style="text-decoration:none" href="#" onclick="window.open(\'https://www.youtube.com/watch?v=' + item.snippet.resourceId.videoId + '&list=PLVvoFIJvWUtYvBbvnq7dZUzza3cyFLv-2\', \'_system\'); return false">' 
      + '<img class="resize" src="' + item.snippet.thumbnails.medium.url + '"/><br />' + item.snippet.title + '</a><hr /></div></center>'
-  
-   $('.container-ministri').append(content);
-
+     $('.container-ministri').append(content);
    })
   });
