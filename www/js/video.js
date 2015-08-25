eb464df328a8a714471030fa34f069c1bc9ef8f4
Iulia Ionita <iulia@ionita.me>
2015-08-25 16:23:20 +0300

    Modificat meniul si rezolvat pagina de video

--- www/js/video.js
+++ www/js/video.js
@@ -1,27 +1,16 @@
 
 myurl = 'https://www.googleapis.com/youtube/v3/playlistItems?playlistId=PLVvoFIJvWUtYvBbvnq7dZUzza3cyFLv-2&key=AIzaSyAatoMZBB2i0T8BcdgIx20xsypdNVhphPc&fields=items&part=snippet&maxResults=20';
 $.ajax({
-  dataType: "json",
+  dataType: "jsonp",
   url: myurl,
   type: 'GET',
   }).done(function ( data ) {
    $.each(data.items, function(i, item){
-    console.log(data.items)
    content =  '<center><div class="nume">'
    + '<a href="https://www.youtube.com/watch?v=' + item.snippet.resourceId.videoId + '&list=PLVvoFIJvWUtYvBbvnq7dZUzza3cyFLv-2">' 
      + '<img class="resize" src="' + item.snippet.thumbnails.medium.url + '"/><br />' + item.snippet.title + '</a><hr /></div></center>'
   
-   $(document).on("collapsibleexpand", "[data-role=collapsible]", function () {
-  var position = $(this).offset().top - 60;
-  $.mobile.silentScroll(position);
-	});
-  
    $('.container-ministri').append(content);
 
-   $('.container-ministri').collapsibleset('refresh');
-	
-	
-	
-	
    })
   });
